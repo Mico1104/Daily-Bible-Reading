9ff6b0cd27f462d12358df8f137b6a2c5efa4e77
Mico1104 <michealmarvellous782@gmail.com>
2025-12-17 14:49:53 +0100

    update Script/index.js

---
+++ Script/index.js
@@ -0,0 +1,6 @@
+const signinBtn = document.getElementById('signinBtn');
+const signupBtn = document.getElementById('signupBtn');
+const title = document.querySelector('.title');
+const nameField = document.getElementById('nameField');
+const phonenumberField = document.getElementById('phone-numberField');
+
